067b978f96d59bcba7d3e1a3c727189d32ff6dc7
Jake Herrera <jkherrera96@gmail.com>
2023-10-22 18:21:07 -0600

    redux toolkit added

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import JournalApp from './JournalApp.tsx'
 import { BrowserRouter } from 'react-router-dom';
 import './styles.css';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>,
   </React.StrictMode>,
 );
